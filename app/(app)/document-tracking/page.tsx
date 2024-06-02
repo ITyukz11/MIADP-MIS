e6d7916fa3d7d25fdd68bef5a627afe2e216ff25
ITyukz11 <it.era0211@gmail.com>
2024-06-02 11:51:51 +0800

    Added Document tracking route

---
+++ app/(app)/document-tracking/page.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client";
+import { Textarea } from "@/components/ui/textarea";
+import { useChat } from "ai/react";
+import { Button } from "@/components/ui/button";
+import { Send } from "lucide-react";
+import { useRef } from "react";
+
+function page() {
+  return (
+    <div >
+  </div>
+  );
+}
+
+export default page;

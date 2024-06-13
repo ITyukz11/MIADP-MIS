00c646bc88f85100d45c74d0d4f30d2a77798fd3
ITyukz11 <it.era0211@gmail.com>
2024-06-13 15:15:46 +0800

    Changed from string of message to * of message only

--- schemas/calendar-of-activity.ts
+++ schemas/calendar-of-activity.ts
@@ -2,11 +2,11 @@ import * as z from "zod";
 
 export const CalendarOfActivitySchema = z.object({
   authorizeOther: z.boolean(),
-  activityTitle:z.string().min(1, "Activity Title is required"),
-  activityDescription: z.string().min(1, "Activity Description is required"),
-  type: z.string().min(1, "Type is required"),
-  targetParticipant: z.string().min(1, "Target Participant is required"),
-  location: z.string().min(1, "Location is required"),
+  activityTitle:z.string().min(1, "*"),
+  activityDescription: z.string().min(1, "*"),
+  type: z.string().min(1, "*"),
+  targetParticipant: z.string().min(1, "*"),
+  location: z.string().min(1, "*"),
   dateFrom: z.string(),
   dateTo: z.string(),
   timeStart: z.string(),
@@ -16,13 +16,14 @@ export const CalendarOfActivitySchema = z.object({
     userId: z.string(),
   }).array(),
   preparatoryList: z.object({
-    description: z.string().min(1, ""),
-    status: z.string().min(1, ""),
+    description: z.string().min(1, "*"),
+    status: z.string().min(1, "*"),
     remarks: z.string(),
   }).array(),
   remarks: z.string(),
   color:z.string(),
   status: z.string(),
+  attachments:z.string(),
   name: z.string()
 });
 

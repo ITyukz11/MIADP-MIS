2f69412d9d8ffd96e513f5b08e1753bb41857cc6
ITyukz11 <it.era0211@gmail.com>
2024-06-13 15:15:10 +0800

    Added miadp pso logo while components are still on loading state

--- app/Loading.tsx
+++ app/Loading.tsx
@@ -1,11 +1,13 @@
 import { LoadingSpinner } from "@/components/LoadingSpinner";
 import { Label } from "@/components/ui/label";
+import Image from "next/image";
 
 const Loading: React.FC = () => {
   return (
     <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh" }}>
       <LoadingSpinner />
       <Label style={{ marginLeft: "8px", fontSize: "1.2rem" }}>Loading...</Label>
+      <Image className="ml-4" src='/miadp-pso-circle.png' alt="miadp-pso logo" width={50} height={50}/> 
     </div>
   );
 };

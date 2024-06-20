4baf51cc803895d5cf5db594c4e5ebaad387e52f
ITyukz11 <it.era0211@gmail.com>
2024-06-20 16:30:54 +0800

    Fixed ehader padding in mobile device

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -6,12 +6,13 @@ import { DropDownMenuComponent } from "./drop-down-menu";
 import { MdNotifications } from "react-icons/md";
 import { useCurrentUser } from "./context/CurrentUserContext";
 import NotificationLottieAnimation from "./lottie-icon-animations/Notification";
+import { Label } from "./ui/label";
 
 export const SiteHeader = () => {
     const { currentUser } = useCurrentUser();
     return (
         <header className="sticky top-0 overflow-hidden z-50 w-full border-b-2 border-border/80 bg-black backdrop-blur supports-[backdrop-filter]:bg-background/60">
-            <div className="px-8 w-full flex h-14 w-min-[320px] items-center gap-2">
+            <div className="px-2 sm:px-8 w-full flex h-14 w-min-[320px] items-center gap-2">
                 <MainNav />
                 <MobileNav />
                 <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
@@ -25,7 +26,7 @@ export const SiteHeader = () => {
                             loop={false}
                             autoplay={false}
                         />
-                        Welcome back {currentUser?.name?.split(' ')[0]}
+                       <Label> Welcome back {currentUser?.name?.split(' ')[0]}</Label>
                         <DropDownMenuComponent />
                         <ModeToggle />
                     </nav>

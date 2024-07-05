9b5e5ac4d7052822394729d45ef4efc1d67e313c
ITyukz11 <it.era0211@gmail.com>
2024-07-05 10:11:28 +0800

    Modified to used the redux feature

--- app/(app)/admin/account/page.tsx
+++ app/(app)/admin/account/page.tsx
@@ -2,44 +2,36 @@
 import { Separator } from "@/components/ui/separator";
 import { useEffect, useState } from "react";
 import { LoadingSpinner } from "@/components/LoadingSpinner";
-import { fetchPendingUsers } from "@/lib/admin/pending-users";
 import PendingAccountForm from "./components/pending-account-form";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { PendingUserType } from "@/components/table/data/pending-users/schema";
+import { usePendingUsersData } from "@/lib/admin/usePendingUsersData";
 
 export default function SettingsAccountPage() {
-  const [loading, setLoading] = useState(true);
-  
   const [filteredPendingUsers, setFilteredPendingUsers] = useState<PendingUserType[]>([]);
   const [pendingUsers, setPendingUsers] = useState<PendingUserType[] | null>(null);
-  
-  const [showAll, setShowAll] = useState(false)
+  const [showAll, setShowAll] = useState(false);
+
+  const { pendingUsersData, pendingUsersError, pendingUsersLoading } = usePendingUsersData();
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await fetchPendingUsers();
+    if (pendingUsersData) {
+      const sortedData = pendingUsersData.sort(
+        (a: any, b: any) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+      );
 
-        const sortedData =data.sort((a:any, b:any) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+      const filteredData = pendingUsersData.filter(
+        (user: PendingUserType) => user.status.toLowerCase() === "pending"
+      );
+      const sortedFilteredData = filteredData.sort(
+        (a: any, b: any) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+      );
 
-        const filteredData = data.filter((user: PendingUserType) => user.status.toLowerCase() === "pending");
-        const sortedFilteredData = filteredData.sort((a:any, b:any) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
-
-        setPendingUsers(sortedData);
-        setFilteredPendingUsers(sortedFilteredData);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching pending users:", error);
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-    
-  
-
+      setPendingUsers(sortedData);
+      setFilteredPendingUsers(sortedFilteredData);
+    }
+  }, [pendingUsersData]);
 
   return (
     <div className="space-y-6">
@@ -47,25 +39,24 @@ export default function SettingsAccountPage() {
         <h3 className="text-lg font-medium">Account</h3>
         <p className="text-sm text-muted-foreground flex justify-between">
           {showAll ? 'These are all the user accounts' : 'There are pending account approvals awaiting confirmation. '}
-          <Button size={'sm'} onClick={() => setShowAll(!showAll)} disabled={loading}>{showAll ? 'Show Pending Users' : 'Show All'}</Button>
+          <Button size={'sm'} onClick={() => setShowAll(!showAll)} disabled={pendingUsersLoading}>
+            {showAll ? 'Show Pending Users' : 'Show All'}
+          </Button>
         </p>
       </div>
       <Separator />
 
-      {loading ? (
+      {pendingUsersLoading ? (
         <div className="flex flex-row gap-2">
           <LoadingSpinner /> Please wait ...
         </div>
-      ) : (
-        pendingUsers && pendingUsers.length > 0 ? (
-          <PendingAccountForm pendingUsers={showAll ? pendingUsers : filteredPendingUsers} disableApproveButton={showAll} />
-        ) : (
-          <div>
-            <Label>Something went wrong.</Label>
-          </div>
-        )
-      )}
-
+      ) : pendingUsersError ? (
+        <div>
+          <Label>Error: {pendingUsersError.message}</Label>
+        </div>
+      ) : pendingUsers && (
+        <PendingAccountForm pendingUsers={showAll ? pendingUsers : filteredPendingUsers} disableApproveButton={showAll} />
+      ) }
     </div>
   );
 }

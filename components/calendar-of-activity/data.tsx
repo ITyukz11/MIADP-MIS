758ce65390ddf6ef59bc979cf47d703dffbf3feb
ITYukz1121 <marymemariyam@gmail.com>
2025-01-15 16:37:11 +0800

    Added Supporting Activity

--- components/calendar-of-activity/data.tsx
+++ components/calendar-of-activity/data.tsx
@@ -1,29 +1,30 @@
-export const ActivityStatus=[
-    { label: "Upcoming", value: "Upcoming" },
-    { label: "Ongoing", value: "Ongoing" },
-    { label: "Completed", value: "Completed" },
-    { label: "Cancelled", value: "Cancelled" },
-    { label: "Postponed", value: "Postponed" }
-]
+export const ActivityStatus = [
+  { label: "Upcoming", value: "Upcoming" },
+  { label: "Ongoing", value: "Ongoing" },
+  { label: "Completed", value: "Completed" },
+  { label: "Cancelled", value: "Cancelled" },
+  { label: "Postponed", value: "Postponed" },
+];
 
-export const PreparatoryActivityStatus=[
-    { label: "Conducted", value: "Conducted" },
-    { label: "To Be Conducted", value: "To Be Conducted" },
-    { label: "Postponed", value: "Postponed" },
-    { label: "Rescheduled", value: "Rescheduled" },
-    { label: "Cancelled", value: "Cancelled" },
-    { label: "Others", value: "Other" },
-]
+export const PreparatoryActivityStatus = [
+  { label: "Conducted", value: "Conducted" },
+  { label: "To Be Conducted", value: "To Be Conducted" },
+  { label: "Postponed", value: "Postponed" },
+  { label: "Rescheduled", value: "Rescheduled" },
+  { label: "Cancelled", value: "Cancelled" },
+  { label: "Others", value: "Other" },
+];
 
-export const TypeData=[
-    { label: "Meeting", value: "Meeting" },
-    { label: "Training", value: "Training" },
-    { label: "Training Workshop", value: "Training Workshop" },
-    { label: "Consultation", value: "Consultation" },
-    { label: "Orientation", value: "Orientation" },
-    { label: "ADAIF Presentation", value: "ADAIF Presentation" },
-    { label: "Community Validation", value: "Community Validation" },
-    { label: "Sub Project Deliberation", value: "Sub Project Deliberation" },
-    { label: "ISM", value: "ISM" },
-    { label: "Other", value: "Other" }
-]
+export const TypeData = [
+  { label: "Meeting", value: "Meeting" },
+  { label: "Training", value: "Training" },
+  { label: "Training Workshop", value: "Training Workshop" },
+  { label: "Consultation", value: "Consultation" },
+  { label: "Orientation", value: "Orientation" },
+  { label: "Supporting Activity", value: "Supporting Activity" },
+  { label: "ADAIF Presentation", value: "ADAIF Presentation" },
+  { label: "Community Validation", value: "Community Validation" },
+  { label: "Sub Project Deliberation", value: "Sub Project Deliberation" },
+  { label: "ISM", value: "ISM" },
+  { label: "Other", value: "Other" },
+];

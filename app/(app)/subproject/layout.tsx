3247be1cfc0ca1dc9cafe472cad129717df55609
ITYukz1121 <marymemariyam@gmail.com>
2025-03-21 14:00:03 +0800

    Removed under development subproject

--- app/(app)/subproject/layout.tsx
+++ app/(app)/subproject/layout.tsx
@@ -1,25 +1,23 @@
-'use client'
-import { Label } from '@radix-ui/react-label'
+"use client";
+import { Label } from "@radix-ui/react-label";
 
 interface ActivitiesLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function SubProjectLayout({ children }: ActivitiesLayoutProps) {
   return (
     <div className="xs:container space-y-2">
-      <div className='flex flex-row flex-wrap justify-center gap-2 md:justify-between'>
-        <div className='flex flex-col'>
-          <div className='flex flex-row gap-2 items-center'>
-            <h2 className="text-xl md:text-3xl font-bold tracking-tight">Subproject </h2>
+      <div className="flex flex-row flex-wrap justify-center gap-2 md:justify-between">
+        <div className="flex flex-col">
+          <div className="flex flex-row gap-2 items-center">
+            <h2 className="text-xl md:text-3xl font-bold tracking-tight">
+              Subproject{" "}
+            </h2>
           </div>
-          <Label className='text-xs sm:text-sm text-muted-foreground'>
-            Underdevelopment
-          </Label>
         </div>
       </div>
       {children}
     </div>
-  )
+  );
 }
-

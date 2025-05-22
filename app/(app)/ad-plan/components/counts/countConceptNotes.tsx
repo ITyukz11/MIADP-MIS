3661e8277db9cfc400bd1455bc8e76eee1289303
ITYukz1121 <marymemariyam@gmail.com>
2025-05-22 15:40:15 +0800

    Remove unused components and files related to the AD Plan and infrastructure, including page layouts, forms, and multi-select filters. This cleanup enhances project maintainability and reduces unnecessary code clutter.

--- app/(app)/ad-plan/components/counts/countConceptNotes.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-export const countConceptNoteTrue = (data: any[]) => {
-  if (!Array.isArray(data)) return 0; // Prevent error if data is undefined or not an array
-  return data.filter((item) => item?.conceptNote === true).length;
-};
--- app/(app)/ad-plan/components/counts/countValidated.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-export const countValidatedTrue = (data: any[]) => {
-  if (!Array.isArray(data)) return 0;
-  return data.filter((item) => item?.validated === true).length;
-};
